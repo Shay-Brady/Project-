6aaf91de2b8fcc44651dfa906ae363c9faec5b20
Shay Brady <100578054+Shay-Brady@users.noreply.github.com>
2022-04-01 10:54:16 +0100

    Add files via upload

---
+++ Students ( Most Challenging ).py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from collections import Counter
+df=pd.read_excel("Students.xlsx")
+list1 = df["most_challenging"].tolist()
+
+b=[]
+for element in list1:
+    element = str(element)
+    b.append(element.split(";"))
+print(b)
+
+a = pd.Series( (v[0] for v in b) )
+
+count={}
+for item in a:
+    if item in count:
+        count[item]+=1
+    else:
+        count[item]=1
+
+
+x=list(count.keys())
+h=list(count.values())
+plt.barh(range(len(count)), h, tick_label=x)
+plt.show()
