5a65e76ca33d620f8452ae53bdbdbbb095272db1
Shay Brady <100578054+Shay-Brady@users.noreply.github.com>
2022-04-04 12:00:52 +0100

    Update and rename Students ( 58 minute classes Adv)py to Students ( 58 minute classes Adv).py

--- Students ( 58 minute classes Adv).py
+++ Students ( 58 minute classes Adv).py
@@ -1,9 +1,10 @@
+#import packages
 import matplotlib.pyplot as plt
 import pandas as pd
 from collections import Counter
 df=pd.read_excel("Students.xlsx")
 list1 = df["What are the main advantages to 58 minute classes for you? You may select multiple answers."].tolist()
-
+#split data
 b=[]
 for element in list1:
     element = str(element)
@@ -11,18 +12,18 @@ for element in list1:
 print(b)
 
 a = pd.Series( (v[0] for v in b) )
-
+#list
 count={}
 for item in a:
     if item in count:
         count[item]+=1
     else:
         count[item]=1
-
+#data for pie chart
 labels = count.keys()
 sizes = count.values()
 explode = (0, 0, 0, 0, 0, 0, 0, 0)  
-
+#plot pie chart
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=61)
