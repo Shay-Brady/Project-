62b5e7e7d4b7c2d809484e61f26442edf27cedd9
Shay Brady <100578054+Shay-Brady@users.noreply.github.com>
2022-04-04 12:02:49 +0100

    Update Students ( Activity That Help With Your Learning ).py

--- Students ( Activity That Help With Your Learning ).py
+++ Students ( Activity That Help With Your Learning ).py
@@ -1,9 +1,10 @@
+#import packages
 import matplotlib.pyplot as plt
 import pandas as pd
 from collections import Counter
 df=pd.read_excel("Students.xlsx")
 list1 = df["What activities from the list below help you with your learning? You may select multiple answers"].tolist()
-
+#split data
 b=[]
 for element in list1:
     element = str(element)
@@ -11,7 +12,7 @@ for element in list1:
 print(b)
 
 a = pd.Series( (v[0] for v in b) )
-
+#list
 count={}
 for item in a:
     if item in count:
@@ -19,11 +20,11 @@ for item in a:
     else:
         count[item]=1
 
-
+#data for pie chart
 labels = count.keys()
 sizes = count.values()
 explode = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)  
-
+#plot pie chart
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=61)
