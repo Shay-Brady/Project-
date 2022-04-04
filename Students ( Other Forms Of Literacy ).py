eb7aec1d490e36adbe8488cfec41aa6fea9fb885
Shay Brady <100578054+Shay-Brady@users.noreply.github.com>
2022-04-04 12:07:37 +0100

    Update Students ( Other Forms Of Literacy ).py

--- Students ( Other Forms Of Literacy ).py
+++ Students ( Other Forms Of Literacy ).py
@@ -1,9 +1,10 @@
+#import packages
 import matplotlib.pyplot as plt
 import pandas as pd
 from collections import Counter
 df=pd.read_excel("Students.xlsx")
 list1 = df["literacy"].tolist()
-
+#split data
 b=[]
 for element in list1:
     element = str(element)
@@ -11,7 +12,7 @@ for element in list1:
 print(b)
 
 a = pd.Series( (v[0] for v in b) )
-
+#list
 count={}
 for item in a:
     if item in count:
@@ -19,7 +20,7 @@ for item in a:
     else:
         count[item]=1
 
-
+#data and plotting pie chart
 x=list(count.keys())
 h=list(count.values())
 plt.barh(range(len(count)), h, tick_label=x)
