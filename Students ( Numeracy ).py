68ce041109b8fbcdc58cf4d2e76dfcb5528b5ded
Shay Brady <100578054+Shay-Brady@users.noreply.github.com>
2022-04-05 13:19:39 +0100

    Add files via upload

---
+++ Students ( Numeracy ).py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+df=pd.read_excel("Students.xlsx")
+data=df.teams.to_list()
+
+count = {
+    "No" : 0,
+    "Yes" : 0
+    }
+
+for item in data:
+    if item in count:
+        count[item]+=1
+    else:
+        count[item]=1
+        
+
+labels = count.keys()
+sizes = count.values()
+explode = (0.1, 0, 0)  
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=120)
+ax1.axis('equal')
+plt.title("Do you think that your numeracy skills\n" + " have been negatively impacted by Covid?", bbox={'facecolor':'0.8', 'pad':2})
+
+
+
+plt.show()
